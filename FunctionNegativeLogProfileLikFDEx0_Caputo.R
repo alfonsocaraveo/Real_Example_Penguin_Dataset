2a4329f6c004bff16d5348465e34333f82adb95f
alfonsocaraveo <alfonso.caraveo@unison.mx>
2025-07-07 13:02:03 -0700

    Add files via upload

---
+++ FunctionNegativeLogProfileLikFDEx0_Caputo.R
@@ -0,0 +1,17 @@
+FunctionNegativeLogProfileLikFDEx0_Caputo<-function(VecPar234,VecTimeObs,VecObs,h,m0,VecPar1)
+{
+  VecPar<-c(VecPar1,VecPar234)
+  PopulationGrowthModel<-c()
+  n<-length(VecTimeObs)
+  Sol <- FunctionPopulationGrowthFDE_Caputo(VecPar,h,m0)
+  final_time<- h*m0
+  time <- seq(0,final_time,h)
+  for (i in 1:n) {
+    idx <- which.min(abs(time - VecTimeObs[i]))
+    PopulationGrowthModel[i] <- Sol[idx]
+    #print(PopulationGrowthModel[i])
+  }
+  vtau<-n/sum(((VecObs-PopulationGrowthModel)/PopulationGrowthModel)^2)
+  LogLik<-(n/2)*log(vtau)-sum(log(PopulationGrowthModel))
+  return(-LogLik)
+}
